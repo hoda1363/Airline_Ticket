f4c7c54009f4137d32ff1d97743663dd8794935e
Asus <hodaghiasi1363@gmail.com>
2024-06-18 17:49:38 +0330

    ui updated

--- src/main/java/mft/controller/PersonController.java
+++ src/main/java/mft/controller/PersonController.java
@@ -42,98 +42,98 @@ public class PersonController implements Initializable {
                 hashCode();
             } catch (Exception e) {
                 Alert alert = new
-                        Alert(Alert . AlertType.ERROR,"new person loud error\n" + e.getMessage());
+                        Alert(Alert.AlertType.ERROR, "new person loud error\n" + e.getMessage());
                 alert.show();
 
             }
         });
-closeMnu.setOnAction(event -> {
-    Alert alert = new
-            Alert(Alert.AlertType.CONFIRMATION, "Are you shure ?");
-    if (alert.showAndWait().get().equals(ButtonType.OK)) {
-        Platform.exit();
-    }
-log . info("App Closed");
+        closeMnu.setOnAction(event -> {
+            Alert alert = new
+                    Alert(Alert.AlertType.CONFIRMATION, "Are you shure ?");
+            if (alert.showAndWait().get().equals(ButtonType.OK)) {
+                Platform.exit();
+            }
+            log.info("App Closed");
 
-});
-saveBtn . setOnAction(event -> {
-    RadioButton role = (RadioButton)
-            roleToggle.getSelectedToggle();
+        });
+        saveBtn.setOnAction(event -> {
+            RadioButton role = (RadioButton)
+                    roleToggle.getSelectedToggle();
 
-    try {
-        Person person = new Person();
+            try {
+                Person person = new Person();
                 .builder()
-                .name(nameTxt.getText())
-                .family(famillyTxt.getText())
-                .nationalid(nationalcodTxt.getText())
-                .birthdate(birthDatepeaker.getpeaker)//todo
-                .build();
-        PersonBl.getPersonBl().save(person);
-        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Person Save\n" + person);
-        alert.show();
-        hashCode();
-        log.info("Person Save " + person);
-    } catch (Exception e) {
-        Alert alert = new Alert(Alert.AlertType.ERROR, "Person Save Error\n" + e.getMessage());
-        alert.show();
-        log.error("Person Save Error" + e.getMessage());
-    }
-});
-editBtn . setOnAction(event -> {
-    RadioButton RadioButton;
-    RadioButton role = (RadioButton)
-    roleToggle.getSelectedToggle();
-    try{
-        Person person = new Person();
+                        .name(nameTxt.getText())
+                        .family(famillyTxt.getText())
+                        .nationalid(nationalcodTxt.getText())
+                        .birthdate(birthDate.getValue())
+                        .build();
+                PersonBl.getPersonBl().save(person);
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Person Save\n" + person);
+                alert.show();
+                hashCode();
+                log.info("Person Save " + person);
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Person Save Error\n" + e.getMessage());
+                alert.show();
+                log.error("Person Save Error" + e.getMessage());
+            }
+        });
+        editBtn.setOnAction(event -> {
+            RadioButton RadioButton;
+            RadioButton role = (RadioButton)
+                    roleToggle.getSelectedToggle();
+            try {
+                Person person = new Person();
          .builder()
-                .name(nameTxt.getText())
-                .family(famillyTxt.getText())
-                .nationalid(nationalcodTxt.getText())
-                .birthdate(birthDatepeaker.getpeaker)//todo
-                .build();
-        PersonBl.getPersonBl().save(person);
-        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Person Updated\n" + person);
-        alert.show();
-        hashCode();
-        log.info("Person Updated" + person);
-    } catch (Exception e) {
-        Alert alert = new Alert(Alert.AlertType.ERROR, "Person Edit Error\n" + e.getMessage());
-        alert.show();
-        log.error("Person Edit Error" + e.getMessage());
-    }
-});
-removeBtn.setOnAction(event -> {
-    try {
-        PersonBl.getPersonBl().remove(Integer.parseInt(idTxt.getText()));
-        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Person Removed\n" + idTxt.getText());
-        alert.show();
-        log.info("person Remove" + idTxt.getText());
-        resetForm();
-    } catch (Exception e) {
-        Alert alert = new Alert(Alert.AlertType.ERROR, "person Remove Error\n" + e.getMessage());
-        alert.show();
-        log.error("Person Remove Error" + e.getMessage());
-    }
-});
-findByIdTxt.setOnKeyReleased(event -> {
-    try {
-        showDataOnTable(PersonBl.getPersonBl().findById(Integer.parseInt(findByIdTxt.getText())));
-        log.info("Find By Person Id Succeed " + Integer.parseInt((findByIdTxt.getText())));
-    } catch (Exception e) {
-        Alert alert = new Alert(Alert.AlertType.ERROR, "search Person id Error\n" + e.getMessage());
-        alert.show();
-        log.error("Find By Prson Id Error" + e.getMessage());
-    }
-});
-findByFamillyTxt.setOnKeyReleased(event ->{
-try{
-    showDataOnTble(personBl.getPersonBL().findByFamiily(findByFamillyTxt,getText()));
-log.info("Find Familly"+findByFamillyTxt.getText());}
-catch (Exception e) {
-    Alert alert = new Alert(Alert.AlertType.ERROR."Search Familly Error\n" + e.getMessage());
-    alert.show();
-    log.error("Find By Familly Error " + e.getMessage());
-}
-});
+                        .name(nameTxt.getText())
+                        .family(famillyTxt.getText())
+                        .nationalid(nationalcodTxt.getText())
+                        .birthdate(birthDate.getValue())
+                        .build();
+                PersonBl.getPersonBl().save(person);
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Person Updated\n" + person);
+                alert.show();
+                hashCode();
+                log.info("Person Updated" + person);
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Person Edit Error\n" + e.getMessage());
+                alert.show();
+                log.error("Person Edit Error" + e.getMessage());
+            }
+        });
+        removeBtn.setOnAction(event -> {
+            try {
+                PersonBl.getPersonBl().remove(Integer.parseInt(idTxt.getText()));
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Person Removed\n" + idTxt.getText());
+                alert.show();
+                log.info("person Remove" + idTxt.getText());
+                resetForm();
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "person Remove Error\n" + e.getMessage());
+                alert.show();
+                log.error("Person Remove Error" + e.getMessage());
+            }
+        });
+        findByIdTxt.setOnKeyReleased(event -> {
+            try {
+                showDataOnTable(PersonBl.getPersonBl().findById(Integer.parseInt(findByIdTxt.getText())));
+                log.info("Find By Person Id Succeed " + Integer.parseInt((findByIdTxt.getText())));
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "search Person id Error\n" + e.getMessage());
+                alert.show();
+                log.error("Find By Prson Id Error" + e.getMessage());
+            }
+        });
+        findByFamillyTxt.setOnKeyReleased(event -> {
+            try {
+                showDataOnTble(personBl.getPersonBL().findByFamiily(findByFamillyTxt, getText()));
+                log.info("Find Familly" + findByFamillyTxt.getText());
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR."Search Familly Error\n" + e.getMessage());
+                alert.show();
+                log.error("Find By Familly Error " + e.getMessage());
+            }
+        });
 
 
